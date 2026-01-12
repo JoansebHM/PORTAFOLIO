9cce1a81194f106d269d8e2b44dec4602a8a05b2
Sebastian Herrera <sebashm621@gmail.com>
2026-01-12 14:22:24 -0500

    feat: initialize React project with Vite and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex flex-col">
+      <span className="text-9xl font-black -tracking-wider text-primary">
+        <span className="text-purple-300">Cepi</span>Works
+      </span>
+      <span className="text-primary text-5xl font-bold tracking-tight">
+        You imagine it, we bring it to{" "}
+        <span className="text-purple-300">life</span>
+      </span>
+    </div>
+  );
+}
+
+export default App;
